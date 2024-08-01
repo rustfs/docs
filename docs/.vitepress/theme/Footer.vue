e30b08ae55fe6c4474044fa7e85582ef497e0618
cxymds <cxymds@qq.com>
2024-08-01 16:48:50 +0800

    feat：a 链接外链改为 _self

--- docs/.vitepress/theme/Footer.vue
+++ docs/.vitepress/theme/Footer.vue
@@ -1,148 +1,149 @@
 <template>
-    <footer class="pc-footer">
-      <div>
-        <h3>架构支持</h3>
-        <ul>
-          <li><a href="https://rustfs.com/html/baremetal.html">裸金属和虚拟化</a></li>
-          <li><a href="https://rustfs.com/html/aliyun.html">阿里云</a></li>
-          <li><a href="https://rustfs.com/html/qcloud.html">腾讯云</a></li>
-          <li><a href="https://rustfs.com/html/huaweicloud.html">华为云</a></li>
-          <li><a href="https://rustfs.com/html/tanzu.html">VMWare Tanzu</a></li>
-          <li><a href="https://rustfs.com/html/aws-elastic.html">国际云厂商</a></li>
-        </ul>
-      </div>
-      <div>
-        <h3>产品功能</h3>
-        <ul>
-          <li><a href="https://rustfs.com/html/solution-distributed.html">分布式</a></li>
-          <li><a href="https://rustfs.com/html/solution-logs.html">日志管理</a></li>
-          <li><a href="https://rustfs.com/html/solution-versions.html">版本控制</a></li>
-          <li><a href="https://rustfs.com/html/solution-s3.html">S3兼容</a></li>
-          <li><a href="https://rustfs.com/html/solution-WORM.html">对象锁与只读</a></li>
-          <li><a href="https://rustfs.com/html/solution-copy.html">跨区域复制</a></li>
-          <li><a href="https://rustfs.com/html/solution-encryption.html">加密</a></li>
-          <li><a href="https://rustfs.com/html/solution-lifecycle.html">生命周期管理</a></li>
-        </ul>
-      </div>
-      <div>
-        <h3>解决方案</h3>
-        <ul>
-          <li><a href="https://rustfs.com/html/product-dataLake.html">现代数据湖</a></li>
-          <li><a href="https://rustfs.com/html/product-AI.html">AI和机器学习</a></li>
-          <li><a href="https://rustfs.com/html/product-cloud.html">云原生</a></li>
-          <li><a href="https://rustfs.com/html/product-HDFS.html">大数据计算存储分离</a></li>
-          <li><a href="https://rustfs.com/html/product-SQL.html">SQL支持</a></li>
-          <li><a href="https://rustfs.com/html/product-quantify.html">量化交易</a></li>
-          <li><a href="https://rustfs.com/html/industry.html">制造业降本</a></li>
-          <li><a href="https://rustfs.com/html/cold-archiving.html">冷归档存储</a></li>
-          <li><a href="https://rustfs.com/html/video.html">视频存储方案</a></li>
-          <li><a href="https://rustfs.com/html/domestic.html">国产信创和SM解决方案</a></li>
-        </ul>
-      </div>
-      <div>
-        <h3>关于我们</h3>
-        <ul>
-          <li><a href="https://rustfs.com/html/about.html">关于我们</a></li>
-          <li><a href="https://rustfs.com/html/about.html#money">投资RustFS</a></li>
-          <li><a href="https://rustfs.com/html/trademark.html">商标使用</a></li>
-          <li><a href="https://github.com/rustfs/rustfs">RustFS GitHub</a></li>
-        </ul>
-      </div>
-      <div class="last">
-        <h3>联系我们</h3>
-        <ul>
-          <li><a href="javascript:;">hello@rusfs.com</a></li>
-          <li><a href="javascript:;">4008-333-333</a></li>
-          <li><a href="javascript:;">北京市海淀区宝盛北里中关村智诚科创大厦3楼</a></li>
-          <li><a href="javascript:;">客服时间：9:00-18:00</a></li>
-        </ul>
-      </div>
-    </footer>
-    <div class="footer">
-      <div class="footer-line">
-        京ICP备2024061305号-1
-        <a class="git-icon" href="https://github.com/rustfs/rustfs">
-          <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github">
-            <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-          </svg>
-        </a>
-      </div>
+  <footer class="pc-footer">
+    <div>
+      <h3>架构支持</h3>
+      <ul>
+        <li><a target="_self" href="https://rustfs.com/html/baremetal.html">裸金属和虚拟化</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/aliyun.html">阿里云</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/qcloud.html">腾讯云</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/huaweicloud.html">华为云</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/tanzu.html">VMWare Tanzu</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/aws-elastic.html">国际云厂商</a></li>
+      </ul>
     </div>
+    <div>
+      <h3>产品功能</h3>
+      <ul>
+        <li><a target="_self" href="https://rustfs.com/html/solution-distributed.html">分布式</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/solution-logs.html">日志管理</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/solution-versions.html">版本控制</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/solution-s3.html">S3兼容</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/solution-WORM.html">对象锁与只读</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/solution-copy.html">跨区域复制</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/solution-encryption.html">加密</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/solution-lifecycle.html">生命周期管理</a></li>
+      </ul>
+    </div>
+    <div>
+      <h3>解决方案</h3>
+      <ul>
+        <li><a target="_self" href="https://rustfs.com/html/product-dataLake.html">现代数据湖</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/product-AI.html">AI和机器学习</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/product-cloud.html">云原生</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/product-HDFS.html">大数据计算存储分离</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/product-SQL.html">SQL支持</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/product-quantify.html">量化交易</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/industry.html">制造业降本</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/cold-archiving.html">冷归档存储</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/video.html">视频存储方案</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/domestic.html">国产信创和SM解决方案</a></li>
+      </ul>
+    </div>
+    <div>
+      <h3>关于我们</h3>
+      <ul>
+        <li><a target="_self" href="https://rustfs.com/html/about.html">关于我们</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/about.html#money">投资RustFS</a></li>
+        <li><a target="_self" href="https://rustfs.com/html/trademark.html">商标使用</a></li>
+        <li><a target="_self" href="https://github.com/rustfs/rustfs">RustFS GitHub</a></li>
+      </ul>
+    </div>
+    <div class="last">
+      <h3>联系我们</h3>
+      <ul>
+        <li><a target="_self" href="javascript:;">hello@rusfs.com</a></li>
+        <li><a target="_self" href="javascript:;">4008-333-333</a></li>
+        <li><a target="_self" href="javascript:;">北京市海淀区宝盛北里中关村智诚科创大厦3楼</a></li>
+        <li><a target="_self" href="javascript:;">客服时间：9:00-18:00</a></li>
+      </ul>
+    </div>
+  </footer>
+  <div class="footer">
+    <div class="footer-line">
+      京ICP备2024061305号-1
+      <a target="_self" class="git-icon" href="https://github.com/rustfs/rustfs">
+        <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github">
+          <path
+            d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+        </svg>
+      </a>
+    </div>
+  </div>
 </template>
 <style>
 .pc-footer {
-    margin-top: 50px;
+  margin-top: 50px;
 }
 footer {
-    display: flex;
-    width: 1220px;
-    margin: 0 auto;
-    margin-top: 24px;
+  display: flex;
+  width: 1220px;
+  margin: 0 auto;
+  margin-top: 24px;
 }
 footer > div {
-    width: 300px;
+  width: 300px;
 }
 footer > div h3 {
-    font-size: 18px;
-    color: #01081a;
-    line-height: 28px;
-    font-weight:bold;
-    margin:18px 0;
+  font-size: 18px;
+  color: #01081a;
+  line-height: 28px;
+  font-weight: bold;
+  margin: 18px 0;
 }
 footer > div ul {
-    list-style: none;
-    margin:18px 0;
-    padding: 0;
+  list-style: none;
+  margin: 18px 0;
+  padding: 0;
 }
 footer > div ul li {
-    height: 32px;
-    line-height: 32px;
+  height: 32px;
+  line-height: 32px;
 }
 footer > div ul li a {
-    font-size: 14px;
-    color: #333333;
-    line-height: 28px;
-    text-decoration: none;
+  font-size: 14px;
+  color: #333333;
+  line-height: 28px;
+  text-decoration: none;
 }
-footer > div ul li a:hover{
-    text-decoration: underline;
+footer > div ul li a:hover {
+  text-decoration: underline;
 }
 footer > div.last {
-    width: 450px;
+  width: 450px;
 }
 footer .last li:nth-of-type(1) {
-    background: url(/images/email.svg) no-repeat left center;
+  background: url(/images/email.svg) no-repeat left center;
 }
 footer .last li:nth-of-type(2) {
-    background: url(/images/tel.svg) no-repeat left center;
+  background: url(/images/tel.svg) no-repeat left center;
 }
 footer .last li:nth-of-type(3) {
-    background: url(/images/location.svg) no-repeat left center;
+  background: url(/images/location.svg) no-repeat left center;
 }
 footer .last li:nth-of-type(4) {
-    background: url(/images/kf.svg) no-repeat left center;
+  background: url(/images/kf.svg) no-repeat left center;
 }
 footer .last li {
-    text-indent: 30px;
+  text-indent: 30px;
 }
 .footer {
-    border-top: 1px solid #ebebeb;
-    height: 66px;
+  border-top: 1px solid #ebebeb;
+  height: 66px;
 }
 .footer-line {
-    height: 66px;
-    width: 1220px;
-    margin: 0 auto;
-    font-size: 14px;
-    color: #333333;
-    line-height: 66px;
+  height: 66px;
+  width: 1220px;
+  margin: 0 auto;
+  font-size: 14px;
+  color: #333333;
+  line-height: 66px;
 }
 .git-icon {
-    float: right;
-    margin-right: 10px;
+  float: right;
+  margin-right: 10px;
 }
 .git-icon svg {
-    vertical-align: middle;
-    display: inline;
+  vertical-align: middle;
+  display: inline;
 }
 </style>
--- docs/.vitepress/theme/HeadTop.vue
+++ docs/.vitepress/theme/HeadTop.vue
@@ -1,60 +1,60 @@
 <template>
   <header :class="{ 'header-bg': scrollYGt100 }">
     <div id="header">
-      <a href="https://rustfs.com/" class="logo"><img src="/images/logo2.svg" class="logo" alt="RustFS首页" /></a>
+      <a target="_self" href="https://rustfs.com/" class="logo"><img src="/images/logo2.svg" class="logo" alt="RustFS首页" /></a>
       <div class="container">
         <div class="menu">
           <div class="login-btn">
-            <a class="down" href="https://rustfs.com/html/download.html">下载</a>
-            <a href="https://rustfs.com/html/price.html">价格</a>
+            <a target="_self" class="down" href="https://rustfs.com/html/download.html">下载</a>
+            <a target="_self" href="https://rustfs.com/html/price.html">价格</a>
           </div>
           <ul>
             <li>
-              <a href="javascript:;" class="list">产品功能</a>
+              <a target="_self" href="javascript:;" class="list">产品功能</a>
               <div class="silideDown first">
                 <div class="list-items">
                   <h3>特征</h3>
                   <div class="item">
-                    <a href="https://rustfs.com/html/solution-s3.html">S3兼容</a>
+                    <a target="_self" href="https://rustfs.com/html/solution-s3.html">S3兼容</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/solution-versions.html">版本控制</a>
+                    <a target="_self" href="https://rustfs.com/html/solution-versions.html">版本控制</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/solution-scalable.html">可扩展</a>
+                    <a target="_self" href="https://rustfs.com/html/solution-scalable.html">可扩展</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/solution-logs.html">日志审计</a>
+                    <a target="_self" href="https://rustfs.com/html/solution-logs.html">日志审计</a>
                   </div>
                 </div>
                 <div class="list-items">
                   <h3>Future</h3>
                   <div class="item">
-                    <a href="https://rustfs.com/html/solution-encryption.html">加密</a>
+                    <a target="_self" href="https://rustfs.com/html/solution-encryption.html">加密</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/solution-lifecycle.html">生命周期管理</a>
+                    <a target="_self" href="https://rustfs.com/html/solution-lifecycle.html">生命周期管理</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/solution-copy.html">桶复制</a>
+                    <a target="_self" href="https://rustfs.com/html/solution-copy.html">桶复制</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/solution-WORM.html">只读</a>
+                    <a target="_self" href="https://rustfs.com/html/solution-WORM.html">只读</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/domestic.html">信创支持</a>
+                    <a target="_self" href="https://rustfs.com/html/domestic.html">信创支持</a>
                   </div>
                 </div>
                 <div class="list-items">
                   <h3>可视化监控</h3>
                   <div class="item">
-                    <a href="https://rustfs.com/html/solution-distributed.html">
+                    <a target="_self" href="https://rustfs.com/html/solution-distributed.html">
                       <div>分布式可扩展</div>
                       <div class="desc">RustFS是非常强劲的分布式存储产品，通过pools去管理分布式集群让分布式集群可以无限向上扩展，轻松实现EB级存储</div>
                     </a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/solution-logs.html">
+                    <a target="_self" href="https://rustfs.com/html/solution-logs.html">
                       <div>监控报警</div>
                       <div class="desc">RustFS可以通过prometheus全面的完成对集群的监控和报警，通过设置alerts规则，实时了解告警状态，保障产生可靠</div>
                     </a>
@@ -63,55 +63,55 @@
                 <div class="list-items">
                   <h3>大数据和AI</h3>
                   <div class="item">
-                    <a href="https://rustfs.com/html/product-AI.html">人工智能</a>
+                    <a target="_self" href="https://rustfs.com/html/product-AI.html">人工智能</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/small-file.html">小文件优化</a>
+                    <a target="_self" href="https://rustfs.com/html/small-file.html">小文件优化</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/product-dataLake.html">数据湖支持</a>
+                    <a target="_self" href="https://rustfs.com/html/product-dataLake.html">数据湖支持</a>
                   </div>
 
                   <div class="item">
-                    <a href="https://rustfs.com/html/product-HDFS.html">存算分离</a>
+                    <a target="_self" href="https://rustfs.com/html/product-HDFS.html">存算分离</a>
                   </div>
                 </div>
               </div>
             </li>
             <li>
-              <a href="javascript:;" class="list">架构</a>
+              <a target="_self" href="javascript:;" class="list">架构</a>
               <div class="silideDown second">
                 <div class="list-items">
                   <h3>虚拟化和国产化支持</h3>
                   <div class="item">
-                    <a href="https://rustfs.com/html/tanzu.html">vmware虚拟化</a>
+                    <a target="_self" href="https://rustfs.com/html/tanzu.html">vmware虚拟化</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/baremetal.html">KVM和国产虚拟化</a>
+                    <a target="_self" href="https://rustfs.com/html/baremetal.html">KVM和国产虚拟化</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/domestic.html">信创和商密支持</a>
+                    <a target="_self" href="https://rustfs.com/html/domestic.html">信创和商密支持</a>
                   </div>
                 </div>
                 <div class="list-items">
                   <h3>云支持</h3>
                   <div class="item">
-                    <a href="https://rustfs.com/html/aliyun.html">阿里云</a>
+                    <a target="_self" href="https://rustfs.com/html/aliyun.html">阿里云</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/qcloud.html">腾讯云</a>
+                    <a target="_self" href="https://rustfs.com/html/qcloud.html">腾讯云</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/huaweicloud.html">华为云</a>
+                    <a target="_self" href="https://rustfs.com/html/huaweicloud.html">华为云</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/aws-elastic.html">国际其他云厂商</a>
+                    <a target="_self" href="https://rustfs.com/html/aws-elastic.html">国际其他云厂商</a>
                   </div>
                 </div>
                 <div class="list-items">
                   <h3>私有云和混合云</h3>
                   <div class="item">
-                    <a href="https://rustfs.com/html/baremetal.html">
+                    <a target="_self" href="https://rustfs.com/html/baremetal.html">
                       <div>裸金属</div>
                       <div class="desc">RustFS可以在私有云和裸金属私有云下完成部署，通过虚批化和裸金属均可构建私通过虚批化和裸金属均可构建私</div>
                     </a>
@@ -120,41 +120,41 @@
                 <div class="list-items">
                   <h3>云原生</h3>
                   <div class="item">
-                    <a href="https://rustfs.com/html/product-cloud.html">Kubernetes</a>
+                    <a target="_self" href="https://rustfs.com/html/product-cloud.html">Kubernetes</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/openshift.html">OpenShift</a>
+                    <a target="_self" href="https://rustfs.com/html/openshift.html">OpenShift</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/tanzu.html">Tanzu</a>
+                    <a target="_self" href="https://rustfs.com/html/tanzu.html">Tanzu</a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/suse-rancher.html">SUSE Rancher</a>
+                    <a target="_self" href="https://rustfs.com/html/suse-rancher.html">SUSE Rancher</a>
                   </div>
                 </div>
               </div>
             </li>
             <li>
-              <a href="javascript:;" class="list">解决方案</a>
+              <a target="_self" href="javascript:;" class="list">解决方案</a>
 
               <div class="silideDown third">
                 <div class="list-items">
                   <div class="item">
-                    <a href="https://rustfs.com/html/product-dataLake.html">
+                    <a target="_self" href="https://rustfs.com/html/product-dataLake.html">
                       <div>现代数据湖</div>
                       <div class="desc">现代数据湖和数据湖仓一体建立在现代对象存储之上。这意味着它们建立在 RustFS 之上。</div>
                     </a>
                   </div>
 
                   <div class="item">
-                    <a href="https://rustfs.com/html/product-cloud.html">
+                    <a target="_self" href="https://rustfs.com/html/product-cloud.html">
                       <div>云原生</div>
                       <div class="desc">混合/多云架构可实现一致的性能、安全性 和经济性。任何关于多云的讨论都需要从一个定义开始。 它不仅仅是一个单一的公共云和本地。</div>
                     </a>
                   </div>
 
                   <div class="item">
-                    <a href="https://rustfs.com/html/industry.html">
+                    <a target="_self" href="https://rustfs.com/html/industry.html">
                       <div>制造业降本</div>
                       <div class="desc">制造业通过的AI质检、视频存储、图像存储中需要大量进行高清图片和高清视频的存储，急需降低存储成本。</div>
                     </a>
@@ -162,20 +162,20 @@
                 </div>
                 <div class="list-items">
                   <div class="item">
-                    <a href="https://rustfs.com/html/product-AI.html">
+                    <a target="_self" href="https://rustfs.com/html/product-AI.html">
                       <div>AI和机器学习</div>
                       <div class="desc">我们是高性能对象存储。</div>
                     </a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/product-HDFS.html">
+                    <a target="_self" href="https://rustfs.com/html/product-HDFS.html">
                       <div>HDFS</div>
                       <div class="desc">未来是分解的，S3兼容和Kubernetes原生 - 换句话说， 除了Hadoop HDFS之外的东西。</div>
                     </a>
                   </div>
 
                   <div class="item">
-                    <a href="https://rustfs.com/html/video.html">
+                    <a target="_self" href="https://rustfs.com/html/video.html">
                       <div>视频存储降本方案</div>
                       <div class="desc">在某些重要场景，视频需要存储长达数10年，海量的视频存储占用了大量的空间，急需要降低视频存取时的成本和提升吞吐</div>
                     </a>
@@ -183,20 +183,20 @@
                 </div>
                 <div class="list-items">
                   <div class="item">
-                    <a href="https://rustfs.com/html/product-veevm.html">
+                    <a target="_self" href="https://rustfs.com/html/product-veevm.html">
                       <div>Veeam备份</div>
                       <div class="desc">面向 Veeam Backup and Replication 的高性能对象存储</div>
                     </a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/product-commvault.html">
+                    <a target="_self" href="https://rustfs.com/html/product-commvault.html">
                       <div>康沃备份</div>
                       <div class="desc">用于 Commvault 备份、恢复和复制的高性能对象存储。简单、可伸缩、快、防勒索软件。换句话说，正是你想要的。</div>
                     </a>
                   </div>
 
                   <div class="item">
-                    <a href="https://rustfs.com/html/cold-archiving.html">
+                    <a target="_self" href="https://rustfs.com/html/cold-archiving.html">
                       <div>冷归档存储</div>
                       <div class="desc">使用RustFS分布式对象存储与蓝光存储相结合，将数据在脱电状态下保存数百年，极大的降低数据归档的存储成本。</div>
                     </a>
@@ -204,20 +204,20 @@
                 </div>
                 <div class="list-items">
                   <div class="item">
-                    <a href="https://rustfs.com/html/product-SQL.html">
+                    <a target="_self" href="https://rustfs.com/html/product-SQL.html">
                       <div>SQL支持</div>
                       <div class="desc">利用 RustFS 的强大功能，使用外部表函数和 polybase 在任何云（ 公有云、私有云或边缘云）上运行 SQL Server 2022。</div>
                     </a>
                   </div>
                   <div class="item">
-                    <a href="https://rustfs.com/html/product-quantify.html">
+                    <a target="_self" href="https://rustfs.com/html/product-quantify.html">
                       <div>量化交易</div>
                       <div class="desc">量化交易 文件存储解决方案，如果希望进一步了解，我们可以安排 1:1 会议为你介绍和演示</div>
                     </a>
                   </div>
 
                   <div class="item">
-                    <a href="https://rustfs.com/html/domestic.html">
+                    <a target="_self" href="https://rustfs.com/html/domestic.html">
                       <div>国产信创和SM方案</div>
                       <div class="desc">全面提供从硬件、操作系统到涉密、加密的全套存储解决方案</div>
                     </a>
@@ -225,14 +225,14 @@
                 </div>
               </div>
             </li>
-            <li class="link"><a href="https://rustfs.com/html/integration.html">集成</a></li>
+            <li class="link"><a target="_self" href="https://rustfs.com/html/integration.html">集成</a></li>
             <li class="link">
-              <a href="javascript:;">技术文档</a>
+              <a target="_self" href="javascript:;">技术文档</a>
             </li>
             <!--
             先注释,等完成后开放
-            <li class="link"><a href="javascript:;">博客</a></li>
-            <li class="link"><a href="javascript:;">技术文档</a></li>
+            <li class="link"><a target="_self" href="javascript:;">博客</a></li>
+            <li class="link"><a target="_self" href="javascript:;">技术文档</a></li>
             -->
             <li class="link"><a target="_self" href="https://rustfs.com/html/product-AI.html">AI支持</a></li>
             <li class="link"><a target="_self" href="https://rustfs.com/html/product-dataLake.html">数据湖</a></li>
